13704bd83f8211b9045545e47d0ef199deba9410
overlord111111 <rasimeireruan8@gmail.com>
2026-06-20 01:12:05 -0300

    chore: limpa temp files, atualiza email, .gitignore

--- _test_exec2.py
+++
@@ -1,7 +0,0 @@
-import urllib.request, re
-
-# Reload: create test file, then execute
-body = urllib.request.urlopen('http://localhost:8080/_admin/executar/teste_exec.spt').read().decode()
-m = re.search(r'class="saida">(.+?)</pre>', body, re.DOTALL)
-saida = m.group(1).strip() if m else 'NOT FOUND'
-print('SAIDA:', repr(saida))
